8b86e1bed8d68d21b36c1f2190a1cffebb2e9587
Maaack <marek.belski@gmail.com>
2023-03-12 14:17:23 -0700

    Adds distinction between current and max level.

--- Scripts/GameLog.gd
+++ Scripts/GameLog.gd
@@ -4,6 +4,7 @@ extends Node
 const GAME_LOG_SECTION = "GameLog"
 const TOTAL_GAMES_STARTED = "TotalGamesStarted"
 const MAX_LEVEL_REACHED = "MaxLevelReached"
+const CURRENT_LEVEL = "CurrentLevel"
 
 static func game_started() -> void:
 	var total_games_started = Config.get_config(GAME_LOG_SECTION, TOTAL_GAMES_STARTED, 0)
@@ -13,10 +14,18 @@ static func game_started() -> void:
 static func level_reached(level_number : int) -> void:
 	var max_level_reached = Config.get_config(GAME_LOG_SECTION, MAX_LEVEL_REACHED, 0)
 	max_level_reached = max(level_number, max_level_reached)
+	Config.set_config(GAME_LOG_SECTION, CURRENT_LEVEL, level_number)
 	Config.set_config(GAME_LOG_SECTION, MAX_LEVEL_REACHED, max_level_reached)
 
 static func get_max_level_reached() -> int:
 	return Config.get_config(GAME_LOG_SECTION, MAX_LEVEL_REACHED, 0)
 
+static func get_current_level() -> int:
+	return Config.get_config(GAME_LOG_SECTION, CURRENT_LEVEL, 0)
+
+static func set_current_level(level_number : int) -> void:
+	Config.set_config(GAME_LOG_SECTION, CURRENT_LEVEL, level_number)
+
 static func reset_game_data() -> void:
+	Config.set_config(GAME_LOG_SECTION, CURRENT_LEVEL, 0)
 	Config.set_config(GAME_LOG_SECTION, MAX_LEVEL_REACHED, 0)
