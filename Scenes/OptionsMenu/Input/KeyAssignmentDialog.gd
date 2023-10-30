0d30489f7e78b5deef14cb7e52b5a46983ccf314
Maaack <marek.belski@gmail.com>
2023-10-30 00:14:07 -0700

    Updates key assignment to work with joypad.

--- Scenes/OptionsMenu/Input/KeyAssignmentDialog.gd
+++ Scenes/OptionsMenu/Input/KeyAssignmentDialog.gd
@@ -1,13 +1,8 @@
 extends ConfirmationDialog
 
-var last_input_event : InputEventWithModifiers
+var last_input_event : InputEvent
 
-func _record_key_event(event : InputEventKey):
-	last_input_event = event
-	dialog_text = event.as_text_keycode()
-	get_ok_button().disabled = false
-
-func _record_mouse_event(event : InputEventMouseButton):
+func _record_input_event(event : InputEvent):
 	last_input_event = event
 	dialog_text = event.as_text()
 	get_ok_button().disabled = false
@@ -15,15 +10,19 @@ func _record_mouse_event(event : InputEventMouseButton):
 func _unhandled_input(event):
 	if not visible:
 		return
-	if event is InputEventKey:
-		_record_key_event(event)
-	elif event is InputEventMouseButton and event.is_released():
-		_record_mouse_event(event)
+	if event is InputEventKey or event is InputEventMouseButton or event is InputEventJoypadButton:
+		_record_input_event(event)
+
+func _is_button_released(input_event : InputEvent) -> bool:
+	return (input_event is InputEventMouseButton or input_event is InputEventJoypadButton) and input_event.is_released()
+
+func _is_possible_button_trigger(input_event : InputEvent) -> bool:
+	return (input_event is InputEventMouseButton and input_event.button_index == MOUSE_BUTTON_LEFT) or (input_event is InputEventJoypadButton and input_event.button_index == JOY_BUTTON_A)
 
 func _input(event):
 	if not visible:
 		return
-	if event is InputEventMouseButton and event.is_released():
-		if event.button_index == MOUSE_BUTTON_LEFT:
+	if _is_button_released(event):
+		if _is_possible_button_trigger(event):
 			await(get_tree().create_timer(0.05).timeout)
-		_record_mouse_event(event)
+		_record_input_event(event)
