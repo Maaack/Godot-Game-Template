612e365be73a0b56f9115cbf7da912862b239974
Maaack <marek.belski@gmail.com>
2023-10-30 00:40:43 -0700

    Makes config methods static.

--- Scripts/Config.gd
+++ Scripts/Config.gd
@@ -1,14 +1,15 @@
 extends Node
+class_name Config
 
 const CONFIG_FILE_LOCATION := "user://config.cfg"
 const DEFAULT_CONFIG_FILE_LOCATION := "res://default_config.cfg"
 
-var config_file : ConfigFile
+static var config_file : ConfigFile
 
-func _init():
+static func _init():
 	load_config_file()
 
-func load_config_file() -> void:
+static func load_config_file() -> void:
 	if config_file != null:
 		return
 	config_file = ConfigFile.new()
@@ -21,26 +22,26 @@ func load_config_file() -> void:
 		if save_error:
 			print("save config file failed with error %d" % save_error)
 
-func set_config(section: String, key: String, value) -> void:
+static func set_config(section: String, key: String, value) -> void:
 	load_config_file()
 	config_file.set_value(section, key, value)
 	var save_error : int = config_file.save(CONFIG_FILE_LOCATION)
 	if save_error:
 		print("save config file failed with error %d" % save_error)
 
-func get_config(section: String, key: String, default = null):
+static func get_config(section: String, key: String, default = null):
 	load_config_file()
 	return config_file.get_value(section, key, default)
 
-func has_section(section: String):
+static func has_section(section: String):
 	load_config_file()
 	return config_file.has_section(section)
 
-func erase_section(section: String):
+static func erase_section(section: String):
 	if has_section(section):
 		config_file.erase_section(section)
 
-func get_section_keys(section: String):
+static func get_section_keys(section: String):
 	load_config_file()
 	if config_file.has_section(section):
 		return config_file.get_section_keys(section)
