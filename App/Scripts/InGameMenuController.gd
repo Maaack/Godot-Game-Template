4266e10212d8b8704f9f4114234cb66ac4e91a27
Maaack <marek.belski@gmail.com>
2023-11-07 16:37:58 -0800

    Returns focus after closing pause menu.

--- App/Scripts/InGameMenuController.gd
+++ App/Scripts/InGameMenuController.gd
@@ -2,11 +2,13 @@ extends Node
 
 var current_menu : CanvasLayer
 var saved_mouse_mode : int
+var saved_focus_control
 
 func open_menu(menu_scene : PackedScene, set_pause : bool = true) -> void:
 	if is_instance_valid(current_menu):
 		return
 	saved_mouse_mode = Input.get_mouse_mode()
+	saved_focus_control = get_viewport().gui_get_focus_owner()
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	current_menu = menu_scene.instantiate()
 	get_tree().current_scene.call_deferred("add_child", current_menu)
@@ -17,3 +19,5 @@ func close_menu() -> void:
 		current_menu.queue_free()
 	Input.set_mouse_mode(saved_mouse_mode)
 	get_tree().paused = false
+	if is_instance_valid(saved_focus_control):
+		saved_focus_control.grab_focus()
