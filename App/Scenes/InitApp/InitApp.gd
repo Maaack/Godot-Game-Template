e7d9c685e364b9eeec67a964c57dcb27e2a0a445
Maaack <marek.belski@gmail.com>
2023-11-30 12:20:50 -0800

    Fixes error on init app opening.

--- App/Scenes/InitApp/InitApp.gd
+++ App/Scenes/InitApp/InitApp.gd
@@ -3,10 +3,13 @@ extends Node
 @export_file("*.tscn") var next_scene : String
 @export var use_loading_screen : bool = false
 
-func _ready():
-	AppLog.app_opened()
-	AppSettings.set_from_config_and_window(get_window())
+func _load_next_scene():
 	if use_loading_screen:
 		SceneLoader.load_scene(next_scene)
 	else:
 		get_tree().change_scene_to_file(next_scene)
+
+func _ready():
+	AppLog.app_opened()
+	AppSettings.set_from_config_and_window(get_window())
+	call_deferred("_load_next_scene")
