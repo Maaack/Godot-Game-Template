a04c6486e20f1d2f1507bea8d82ae2648925f04c
Maaack <marek.belski@gmail.com>
2024-03-21 15:49:41 -0700

    Some refactoring of code for readability and consistency.

--- App/Scenes/LoadingScreen/LoadingScreen.gd
+++ App/Scenes/LoadingScreen/LoadingScreen.gd
@@ -30,6 +30,9 @@ func _reset_loading_stage():
 	_stall_stage = StallStage.STARTED
 	%LoadingTimer.start()
 
+func _reset_loading_start_time():
+	_loading_start_time = Time.get_ticks_msec()
+
 func _try_loading_next_scene():
 	if not _scene_loading_complete:
 		return
@@ -44,50 +47,68 @@ func _load_next_scene():
 func _get_seconds_waiting() -> int:
 	return Time.get_ticks_msec() - _loading_start_time / 1000
 
+func _update_scene_loading_progress():
+	var new_progress = SceneLoader.get_progress()
+	if new_progress > _scene_loading_progress:
+		_scene_loading_progress = new_progress
+
+func _set_scene_loading_complete():
+	_scene_loading_progress = 1.0
+	_scene_loading_complete = true
+
+func _reset_scene_loading_progress():
+	_scene_loading_progress = 0.0
+	_scene_loading_complete = false
+
+func _update_in_progress_messaging():
+	match _stall_stage:
+		StallStage.STARTED:
+			%ErrorMessage.hide()
+			%Title.text = LOADING_TEXT
+		StallStage.WAITING:
+			%ErrorMessage.hide()
+			%Title.text = LOADING_TEXT_WAITING
+		StallStage.STILL_WAITING:
+			%ErrorMessage.hide()
+			%Title.text = LOADING_TEXT_STILL_WAITING % _get_seconds_waiting()
+		StallStage.GIVE_UP:
+			if %ErrorMessage.visible:
+				return
+			if _scene_loading_progress == 0:
+				%ErrorMessage.dialog_text = "Loading Error: Stalled at start."
+				if OS.has_feature("web"):
+					%ErrorMessage.dialog_text += "\nTry refreshing the page."
+			else:
+				%ErrorMessage.dialog_text = "Loading Error: Stalled at %d%%." % (_scene_loading_progress * 100.0)
+			%ErrorMessage.popup_centered()
+
+func _update_loaded_messaging():
+	match _stall_stage:
+		StallStage.STARTED:
+			%ErrorMessage.hide()
+			%Title.text = LOADING_COMPLETE_TEXT
+		StallStage.WAITING:
+			%ErrorMessage.hide()
+			%Title.text = LOADING_COMPLETE_TEXT_WAITING
+		StallStage.STILL_WAITING:
+			%ErrorMessage.hide()
+			%Title.text = LOADING_TEXT_STILL_WAITING % _get_seconds_waiting()
+		StallStage.GIVE_UP:
+			if %ErrorMessage.visible:
+				return
+			%ErrorMessage.dialog_text = "Loading Error: Failed to switch scenes."
+			%ErrorMessage.popup_centered()
+
 func _process(_delta):
 	_try_loading_next_scene()
 	var status = SceneLoader.get_status()
 	match(status):
 		ResourceLoader.THREAD_LOAD_IN_PROGRESS:
-			_scene_loading_progress = SceneLoader.get_progress()
-			match _stall_stage:
-				StallStage.STARTED:
-					%ErrorMessage.hide()
-					%Title.text = LOADING_TEXT
-				StallStage.WAITING:
-					%ErrorMessage.hide()
-					%Title.text = LOADING_TEXT_WAITING
-				StallStage.STILL_WAITING:
-					%ErrorMessage.hide()
-					%Title.text = LOADING_TEXT_STILL_WAITING % _get_seconds_waiting()
-				StallStage.GIVE_UP:
-					if %ErrorMessage.visible:
-						return
-					if _scene_loading_progress == 0:
-						%ErrorMessage.dialog_text = "Loading Error: Failed to start."
-						if OS.has_feature("web"):
-							%ErrorMessage.dialog_text += "\nTry refreshing the page."
-					else:
-						%ErrorMessage.dialog_text = "Loading Error: Failed at %d%%." % (_scene_loading_progress * 100.0)
-					%ErrorMessage.popup_centered()
+			_update_scene_loading_progress()
+			_update_in_progress_messaging()
 		ResourceLoader.THREAD_LOAD_LOADED:
-			_scene_loading_progress = 1.0
-			_scene_loading_complete = true
-			match _stall_stage:
-				StallStage.STARTED:
-					%ErrorMessage.hide()
-					%Title.text = LOADING_COMPLETE_TEXT
-				StallStage.WAITING:
-					%ErrorMessage.hide()
-					%Title.text = LOADING_COMPLETE_TEXT_WAITING
-				StallStage.STILL_WAITING:
-					%ErrorMessage.hide()
-					%Title.text = LOADING_TEXT_STILL_WAITING % _get_seconds_waiting()
-				StallStage.GIVE_UP:
-					if %ErrorMessage.visible:
-						return
-					%ErrorMessage.dialog_text = "Loading Error: Failed to switch scenes."
-					%ErrorMessage.popup_centered()
+			_set_scene_loading_complete()
+			_update_loaded_messaging()
 		ResourceLoader.THREAD_LOAD_FAILED:
 			%ErrorMessage.dialog_text = "Loading Error: %d" % status
 			%ErrorMessage.popup_centered()
@@ -104,19 +125,20 @@ func _on_loading_timer_timeout():
 			%LoadingTimer.start()
 		StallStage.WAITING:
 			_stall_stage = StallStage.STILL_WAITING
+			%LoadingTimer.start()
 		StallStage.STILL_WAITING:
 			_stall_stage = StallStage.GIVE_UP
 
 func _on_error_message_confirmed():
 	var err = get_tree().change_scene_to_file(ProjectSettings.get_setting("application/run/main_scene"))
 	if err:
-		print("failed to load main scene: %d" % err)
+		push_error("failed to load main scene: %d" % err)
 		get_tree().quit()
 
 func reset():
 	show()
 	_reset_loading_stage()
-	_scene_loading_progress = 0.0
-	_loading_start_time = Time.get_ticks_msec()
+	_reset_scene_loading_progress()
+	_reset_loading_start_time()
 	%ErrorMessage.hide()
 	set_process(true)
