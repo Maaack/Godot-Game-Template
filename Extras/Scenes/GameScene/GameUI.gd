395cf5274ea2ed257df375830949f67f301f89d8
Maaack <marek.belski@gmail.com>
2024-03-21 16:20:04 -0700

    Moves input reading into separate script.

--- Extras/Scenes/GameScene/GameUI.gd
+++ Extras/Scenes/GameScene/GameUI.gd
@@ -2,25 +2,6 @@ extends Control
 
 @export var win_scene : PackedScene
 @export var lose_scene : PackedScene
-@onready var action_names = AppSettings.get_filtered_action_names()
-
-func _get_inputs_as_string():
-	var all_inputs : String = ""
-	var is_first : bool = true
-	for action_name in action_names:
-			if Input.is_action_pressed(action_name):
-				if is_first:
-					is_first = false
-					all_inputs += action_name
-				else:
-					all_inputs += " + " + action_name
-	return all_inputs
-
-func _process(_delta):
-	if Input.is_anything_pressed():
-		$Label.text = _get_inputs_as_string()
-	else:
-		$Label.text = ""
 
 func _ready():
 	InGameMenuController.scene_tree = get_tree()
