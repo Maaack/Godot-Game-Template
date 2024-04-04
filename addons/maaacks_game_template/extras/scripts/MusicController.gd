3a88bbf2b60c923c886346b783a8818d13a32b2b
Maaack <marek.belski@gmail.com>
2024-04-04 16:52:00 -0700

    Adds a fade in option and organizes export vars.

--- addons/maaacks_game_template/extras/scripts/MusicController.gd
+++ addons/maaacks_game_template/extras/scripts/MusicController.gd
@@ -11,16 +11,28 @@ const MINIMUM_VOLUME_DB = -80
 
 @export var root_path : NodePath = ^".."
 @export var audio_bus : StringName = &"Music"
-@export_range(-80, 24) var volume_db : float
-@export var fade_out_duration : float = 0.0
-
 ## Continually check any new nodes added to the scene tree.
 @export var persistent : bool = true :
 	set(value):
 		persistent = value
 		_update_persistent_signals()
 
+@export_group("Playback")
+@export_range(-80, 24) var volume_db : float
 @export var empty_streams_stop_player : bool = true
+
+@export_group("Blending")
+@export var fade_out_duration : float = 0.0 :
+	set(value):
+		fade_out_duration = value
+		if fade_out_duration < 0:
+			fade_out_duration = 0
+			
+@export var fade_in_duration : float = 0.0 :
+	set(value):
+		fade_in_duration = value
+		if fade_in_duration < 0:
+			fade_in_duration = 0
 
 @onready var root_node : Node = get_node(root_path)
 
@@ -55,6 +67,7 @@ func fade_out( duration : float = 0.0 ):
 
 func fade_in( duration : float = 0.0 ):
 	if not is_zero_approx(duration):
+		music_stream_player.volume_db = MINIMUM_VOLUME_DB
 		var tween = get_tree().create_tween()
 		tween.tween_property(music_stream_player, "volume_db", volume_db, duration)
 		return tween
@@ -70,6 +83,12 @@ func play():
 	music_stream_player.volume_db = volume_db
 	music_stream_player.play()
 
+func play_and_fade_in( duration : float = 0.0 ):
+	if music_stream_player == null:
+		return
+	music_stream_player.play()
+	fade_in( duration )
+
 func _is_matching_stream( stream_player : AudioStreamPlayer ) -> bool:
 	if stream_player.bus != audio_bus:
 		return false
@@ -80,7 +99,11 @@ func _is_matching_stream( stream_player : AudioStreamPlayer ) -> bool:
 func reparent_music_player( stream_player : AudioStreamPlayer ):
 	stream_player.call_deferred("reparent", self)
 	music_stream_player = stream_player
-	music_stream_player.volume_db = volume_db
+
+func _blend_in_stream_player( stream_player : AudioStreamPlayer ):
+	fade_out_and_free(fade_out_duration)
+	reparent_music_player(stream_player)
+	play_and_fade_in(fade_in_duration)
 
 func check_for_music_player( node: Node ) -> void:
 	if node == music_stream_player : return
@@ -91,8 +114,9 @@ func check_for_music_player( node: Node ) -> void:
 			if not music_stream_player.playing:
 				play()
 		else:
-			fade_out_and_free(fade_out_duration)
-			reparent_music_player(node)
+			if node.stream == null and not empty_streams_stop_player:
+				return
+			_blend_in_stream_player(node)
 
 func _recursive_check_for_music_player( current_node: Node, current_depth : int = 0 ) -> void:
 	if current_depth >= MAX_DEPTH:
