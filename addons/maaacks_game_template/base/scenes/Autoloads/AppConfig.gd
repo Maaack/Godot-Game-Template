05c91f513cfe1a95b54f0be42a0b25598ed428bb
Maaack <marek.belski@gmail.com>
2024-04-23 22:34:06 -0700

    Removes redundant loading screen var from app config.

--- addons/maaacks_game_template/base/scenes/Autoloads/AppConfig.gd
+++ addons/maaacks_game_template/base/scenes/Autoloads/AppConfig.gd
@@ -1,8 +1,5 @@
 extends Node
 
-@export_file("*.tscn") var _loading_screen : String
-
 func _ready():
 	AppLog.app_opened()
 	AppSettings.set_from_config_and_window(get_window())
-	SceneLoader.set_loading_screen(_loading_screen)
