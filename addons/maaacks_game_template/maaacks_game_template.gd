7dd711dc418525d72683c616fd3fd7fc6aded435
Maaack <marek.belski@gmail.com>
2024-04-23 22:05:15 -0700

    Removes unused autoloads from auto-install.

--- addons/maaacks_game_template/maaacks_game_template.gd
+++ addons/maaacks_game_template/maaacks_game_template.gd
@@ -5,8 +5,6 @@ extends EditorPlugin
 func _enter_tree():
 	add_autoload_singleton("AppConfig", "res://addons/maaacks_game_template/base/scenes/Autoloads/AppConfig.tscn")
 	add_autoload_singleton("SceneLoader", "res://addons/maaacks_game_template/base/scripts/SceneLoader.gd")
-	add_autoload_singleton("RuntimeLogger", "res://addons/maaacks_game_template/extras/scripts/RuntimeLogger.gd")
-	add_autoload_singleton("ProjectLevelLoader", "res://addons/maaacks_game_template/extras/scenes/Autoloads/ProjectLevelLoader.tscn")
 	add_autoload_singleton("ProjectMusicController", "res://addons/maaacks_game_template/extras/scenes/Autoloads/ProjectMusicController.tscn")
 	add_autoload_singleton("ProjectUISoundController", "res://addons/maaacks_game_template/extras/scenes/Autoloads/ProjectUISoundController.tscn")
 
