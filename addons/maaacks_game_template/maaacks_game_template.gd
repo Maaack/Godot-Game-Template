f9c0a8bfadbd71415fcb23d8fedfdef3238f570c
Maaack <marek.belski@gmail.com>
2024-04-25 20:22:43 -0700

    Updates from menu template.

--- addons/maaacks_game_template/maaacks_game_template.gd
+++ addons/maaacks_game_template/maaacks_game_template.gd
@@ -5,8 +5,8 @@ extends EditorPlugin
 func _enter_tree():
 	add_autoload_singleton("AppConfig", "res://addons/maaacks_game_template/base/scenes/Autoloads/AppConfig.tscn")
 	add_autoload_singleton("SceneLoader", "res://addons/maaacks_game_template/base/scenes/Autoloads/SceneLoader.tscn")
-	add_autoload_singleton("ProjectMusicController", "res://addons/maaacks_game_template/extras/scenes/Autoloads/ProjectMusicController.tscn")
-	add_autoload_singleton("ProjectUISoundController", "res://addons/maaacks_game_template/extras/scenes/Autoloads/ProjectUISoundController.tscn")
+	add_autoload_singleton("ProjectMusicController", "res://addons/maaacks_game_template/base/scenes/Autoloads/ProjectMusicController.tscn")
+	add_autoload_singleton("ProjectUISoundController", "res://addons/maaacks_game_template/base/scenes/Autoloads/ProjectUISoundController.tscn")
 
 func _exit_tree():
 	remove_autoload_singleton("AppConfig")
