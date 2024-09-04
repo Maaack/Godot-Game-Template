e79011d3662f1d3a03d02874583086a3d7fc8a83
Maaack <marek.belski@gmail.com>
2024-09-04 16:22:42 -0700

    Starting on preserved app state using resources.

---
+++ addons/maaacks_game_template/base/scripts/app_state_data.gd
@@ -0,0 +1,7 @@
+class_name AppStateData
+extends Resource
+
+@export var first_version_opened : String
+@export var last_version_opened : String
+@export var open_count : int
+@export var states : Array[Resource]
