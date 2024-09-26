523ce2d230c8f2eef2b2c11218689dd1ed3220d9
Marek Belski <marek.belski@gmail.com>
2024-09-25 21:12:47 -0700

    Moves logging of version to app_log.

--- addons/maaacks_game_template/base/scenes/menus/main_menu/config_version_label.gd
+++ addons/maaacks_game_template/base/scenes/menus/main_menu/config_version_label.gd
@@ -12,7 +12,6 @@ func update_version_label():
 	var config_version : String = ProjectSettings.get_setting("application/config/version", NO_VERSION_STRING)
 	if config_version.is_empty():
 		config_version = NO_VERSION_STRING
-	AppLog.version_opened(config_version)
 	text = version_prefix + config_version
 
 func _ready():
