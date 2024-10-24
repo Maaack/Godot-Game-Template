f86dd72ec7bb67192fda25e299a4531a32e1fc33
Marek Belski <marek.belski@gmail.com>
2024-10-23 23:26:12 -0700

    Gives finer control over opening fade in/out animations.

--- addons/maaacks_game_template/extras/scenes/opening/opening.gd
+++ addons/maaacks_game_template/extras/scenes/opening/opening.gd
@@ -2,11 +2,19 @@ extends Control
 
 @export_file("*.tscn") var next_scene : String
 @export var images : Array[Texture2D]
+@export_group("Animation")
+@export var fade_in_time : float = 0.2
+@export var fade_out_time : float = 0.2
+@export var visible_time : float = 1.6
+@export_group("Transition")
 @export var start_delay : float = 0.5
 @export var end_delay : float = 0.5
 @export var show_loading_screen : bool = false
 
-func next():
+var tween : Tween
+var next_image_index : int = 0
+
+func _load_next_scene():
 	var status = SceneLoader.get_status()
 	if show_loading_screen or status != ResourceLoader.THREAD_LOAD_LOADED:
 		SceneLoader.change_scene_to_loading_screen()
@@ -19,7 +27,7 @@ func _add_textures_to_container(textures : Array[Texture2D]):
 		texture_rect.texture = texture
 		texture_rect.stretch_mode = TextureRect.STRETCH_KEEP_ASPECT_CENTERED
 		texture_rect.expand_mode = TextureRect.EXPAND_IGNORE_SIZE
-		texture_rect.visible = false
+		texture_rect.modulate.a = 0.0
 		%ImagesContainer.call_deferred("add_child", texture_rect)
 
 func _event_skips_intro(event : InputEvent):
@@ -32,26 +40,42 @@ func _event_is_mouse_button_released(event : InputEvent):
 
 func _unhandled_input(event):
 	if _event_skips_intro(event):
-		next()
+		_load_next_scene()
 
 func _gui_input(event):
 	if _event_is_mouse_button_released(event):
-		next()
+		_load_next_scene()
 
-func _finished_animating():
-	await(get_tree().create_timer(end_delay).timeout)
-	next()
+func _transition_out():
+	await get_tree().create_timer(end_delay).timeout
+	_load_next_scene()
 
-func _animate_images():
-	for texture_rect in %ImagesContainer.get_children():
-		texture_rect.show()
-		%AnimationPlayer.play("FadeInOut")
-		await(%AnimationPlayer.animation_finished)
-		texture_rect.hide()
-	_finished_animating()
+func _transition_in():
+	await get_tree().create_timer(start_delay).timeout
+	if next_image_index == 0:
+		_show_next_image()
+
+func _wait_and_fade_out(texture_rect : TextureRect):
+	var _compare_next_index = next_image_index
+	await get_tree().create_timer(visible_time, false).timeout
+	if _compare_next_index != next_image_index : return
+	tween = create_tween()
+	tween.tween_property(texture_rect, "modulate:a", 0.0, fade_out_time)
+	await tween.finished
+	_show_next_image.call_deferred()
+
+func _show_next_image():
+	if next_image_index >= %ImagesContainer.get_child_count():
+		_transition_out()
+		return
+	var texture_rect = %ImagesContainer.get_child(next_image_index)
+	tween = create_tween()
+	tween.tween_property(texture_rect, "modulate:a", 1.0, fade_in_time)
+	await tween.finished
+	next_image_index += 1
+	_wait_and_fade_out(texture_rect)
 
 func _ready():
 	SceneLoader.load_scene(next_scene, true)
 	_add_textures_to_container(images)
-	await(get_tree().create_timer(start_delay).timeout)
-	_animate_images()
+	_transition_in()
