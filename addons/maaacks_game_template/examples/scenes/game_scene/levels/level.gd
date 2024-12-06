80eb463d0b0c3c556001b81cefd67205d5cd72c7
Marek Belski <marek.belski@gmail.com>
2024-12-05 20:33:56 -0800

    Updates example levels with example states.

--- addons/maaacks_game_template/examples/scenes/game_scene/levels/level.gd
+++ addons/maaacks_game_template/examples/scenes/game_scene/levels/level.gd
@@ -3,8 +3,22 @@ extends Node
 signal level_won
 signal level_lost
 
+var level_state : LevelStateExample
+
 func _on_lose_button_pressed():
 	emit_signal("level_lost")
 
 func _on_win_button_pressed():
 	emit_signal("level_won")
+
+func _ready():
+	GameStateExample.current_level = get_path()
+	level_state = GameStateExample.get_current_level_state()
+	%ColorPickerButton.color = level_state.color
+	%ColorPickerButton.color_changed.emit(level_state.color)
+
+func _on_color_picker_button_color_changed(color):
+	%ColorPickerButton.color = color
+	%BackgroundColor.color = color
+	level_state.color = color
+	GlobalState.save()
