92c18fc45bd118100a393b21c3e1617f98ac6246
Marek Belski <marek.belski@gmail.com>
2025-01-09 11:43:14 -0800

    Changes condition for showing level select button.

--- addons/maaacks_game_template/examples/scenes/menus/main_menu/main_menu_with_animations.gd
+++ addons/maaacks_game_template/examples/scenes/menus/main_menu/main_menu_with_animations.gd
@@ -37,10 +37,7 @@ func _close_sub_menu():
 	animation_state_machine.travel("OpenMainMenu")
 
 func _setup_level_select(): 
-	if level_select_packed_scene == null:
-		%LevelSelectButton.hide()
-	else:
-		%LevelSelectButton.show()
+	if level_select_packed_scene != null:
 		level_select_scene = level_select_packed_scene.instantiate()
 		level_select_scene.hide()
 		%LevelSelectContainer.call_deferred("add_child", level_select_scene)
@@ -62,6 +59,7 @@ func _setup_game_buttons():
 	super._setup_game_buttons()
 	if GameStateExample.has_game_state():
 		%ContinueGameButton.show()
+		%LevelSelectButton.show()
 
 func _on_continue_game_button_pressed():
 	load_game_scene()
