6ac7471e1d1accbe0a74c9f9c17611b9821e78fd
Marek Belski <marek.belski@gmail.com>
2025-02-27 15:15:17 -0800

    File lister now takes multiple directories.

--- addons/maaacks_game_template/extras/scripts/file_lister.gd
+++ addons/maaacks_game_template/extras/scripts/file_lister.gd
@@ -4,14 +4,14 @@ class_name FileLister
 ## Helper class for listing all the scenes in a directory.
 
 ## List of paths to scene files.
-## Prefilled in the editor by selecting a directory.
+@export_tool_button("Refresh Files") var _refresh_files_action = _refresh_files
+## Filled in the editor by selecting a directory.
 @export var files : Array[String]
-## Prefill files with any scenes in the directory.
-@export_dir var directory : String :
+## Fills files with those discovered in directories, and matching constraints.
+@export_dir var directories : Array[String] :
 	set(value):
-		directory = value
+		directories = value
 		_refresh_files()
-@export_tool_button("Update") var _refresh_files_action = _refresh_files
 
 @export_group("Constraints")
 @export var search : String
@@ -20,20 +20,27 @@ class_name FileLister
 @export_subgroup("Advanced Search")
 @export var begins_with : String
 @export var ends_with : String
+@export var not_begins_with : String
+@export var not_ends_with : String
 
 
 func _refresh_files():
-	if not is_inside_tree() or directory.is_empty(): return
-	var dir_access = DirAccess.open(directory)
-	if dir_access:
-		files.clear()
-		for file in dir_access.get_files():
-			if (not search.is_empty()) and (not file.contains(search)):
-				continue
-			if (not filter.is_empty()) and (file.contains(filter)):
-				continue
-			if (not begins_with.is_empty()) and (not file.begins_with(begins_with)):
-				continue
-			if (not ends_with.is_empty()) and (not file.ends_with(ends_with)):
-				continue
-			files.append(directory + "/" + file)
+	if not is_inside_tree() or directories.is_empty(): return
+	files.clear()
+	for directory in directories:
+		var dir_access = DirAccess.open(directory)
+		if dir_access:
+			for file in dir_access.get_files():
+				if (not search.is_empty()) and (not file.contains(search)):
+					continue
+				if (not filter.is_empty()) and (file.contains(filter)):
+					continue
+				if (not begins_with.is_empty()) and (not file.begins_with(begins_with)):
+					continue
+				if (not ends_with.is_empty()) and (not file.ends_with(ends_with)):
+					continue
+				if (not not_begins_with.is_empty()) and (file.begins_with(not_begins_with)):
+					continue
+				if (not not_ends_with.is_empty()) and (file.ends_with(not_ends_with)):
+					continue
+				files.append(directory + "/" + file)
