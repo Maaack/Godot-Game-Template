761175c06b453efce7fd5bbea150f3806f608bfc
Marek Belski <marek.belski@gmail.com>
2025-04-24 15:34:09 -0700

    Recopies example files into project root.

--- scenes/game_scene/levels/level.gd
+++ scenes/game_scene/levels/level.gd
@@ -11,12 +11,21 @@ func _on_lose_button_pressed() -> void:
 func _on_win_button_pressed() -> void:
 	level_won.emit()
 
+func open_tutorials() -> void:
+	%TutorialManager.open_tutorials()
+	level_state.tutorial_read = true
+
 func _ready() -> void:
 	level_state = GameState.get_level_state(scene_file_path)
 	%ColorPickerButton.color = level_state.color
 	%BackgroundColor.color = level_state.color
+	if not level_state.tutorial_read:
+		open_tutorials()
 
 func _on_color_picker_button_color_changed(color : Color) -> void:
 	%BackgroundColor.color = color
 	level_state.color = color
 	GlobalState.save()
+
+func _on_tutorial_button_pressed() -> void:
+	open_tutorials()
