2bc3a68e303c204fafd465abf61a4d4640272187
Marek Belski <marek.belski@gmail.com>
2025-04-24 13:59:50 -0700

    Makes tutorial viewed state persistent.

--- addons/maaacks_game_template/examples/scenes/game_scene/tutorial_manager.gd
+++ addons/maaacks_game_template/examples/scenes/game_scene/tutorial_manager.gd
@@ -1,8 +1,11 @@
 extends Node
 @export var tutorial_scenes : Array[PackedScene]
-@export var delay : float = 0.25
+@export var open_delay : float = 0.25
+@export var auto_open : bool = false
 
 func open_tutorials() -> void:
+	if open_delay > 0.0:
+		await get_tree().create_timer(auto_open, false).timeout
 	for tutorial_scene in tutorial_scenes:
 		var tutorial_menu : OverlaidMenu = tutorial_scene.instantiate()
 		if tutorial_menu == null:
@@ -12,5 +15,5 @@ func open_tutorials() -> void:
 		await tutorial_menu.tree_exited
 
 func _ready() -> void:
-	await get_tree().create_timer(delay, false).timeout
-	open_tutorials()
+	if auto_open:
+		open_tutorials()
