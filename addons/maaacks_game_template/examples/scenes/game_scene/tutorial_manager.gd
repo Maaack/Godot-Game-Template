5b11e70677a4eb6d4d5ec8ef23c65241d3dba835
Marek Belski <marek.belski@gmail.com>
2025-04-24 12:50:51 -0700

    Adds tutorial manager example.

---
+++ addons/maaacks_game_template/examples/scenes/game_scene/tutorial_manager.gd
@@ -0,0 +1,16 @@
+extends Node
+@export var tutorial_scenes : Array[PackedScene]
+@export var delay : float = 1.0
+
+func open_tutorials():
+	for tutorial_scene in tutorial_scenes:
+		var tutorial_menu : OverlaidMenu = tutorial_scene.instantiate()
+		if tutorial_menu == null:
+			push_warning("tutorial failed to open %s" % tutorial_scene)
+			return
+		get_tree().current_scene.call_deferred("add_child", tutorial_menu)
+		await tutorial_menu.tree_exited
+
+func _ready():
+	await get_tree().create_timer(delay, false).timeout
+	open_tutorials()
