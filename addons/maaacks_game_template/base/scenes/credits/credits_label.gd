e37f66e93d19ee8c564726337f946928d3652396
Marek Belski <marek.belski@gmail.com>
2025-06-05 14:52:00 -0700

    Adds image support to credits label.

--- addons/maaacks_game_template/base/scenes/credits/credits_label.gd
+++ addons/maaacks_game_template/base/scenes/credits/credits_label.gd
@@ -9,6 +9,9 @@ extends RichTextLabel
 @export var h2_font_size: int
 @export var h3_font_size: int
 @export var h4_font_size: int
+@export_group("Image Sizes")
+@export var max_image_width: int
+@export var max_image_height : int
 @export_group("Extra Options")
 ## For platforms that don't permit linking to other domains or products.
 @export var disable_urls: bool
@@ -19,6 +22,21 @@ func load_file(file_path) -> String:
 		push_warning("File open error: %s" % FileAccess.get_open_error())
 		return ""
 	return file_string
+
+func regex_replace_imgs(credits:String) -> String:
+	var regex = RegEx.new()
+	var match_string : String = "!\\[([^\\]]*)\\]\\(([^\\)]*)\\)"
+	var replace_string : String = "res://$2[/img]"
+	if max_image_width:
+		if max_image_height:
+			replace_string = ("[img=%dx%d]" % [max_image_width, max_image_height]) + replace_string
+		else:
+			replace_string = ("[img=%d]" % [max_image_width]) + replace_string
+	else:
+		replace_string = "[img]" + replace_string
+	regex.compile(match_string)
+	regex.get_group_count()
+	return regex.sub(credits, replace_string, true)
 
 func regex_replace_urls(credits:String) -> String:
 	var regex = RegEx.new()
@@ -45,6 +63,7 @@ func _update_text_from_file() -> void:
 		return
 	var _end_of_first_line = file_text.find("\n") + 1
 	file_text = file_text.right(-_end_of_first_line) # Trims first line "ATTRIBUTION"
+	file_text = regex_replace_imgs(file_text)
 	file_text = regex_replace_urls(file_text)
 	file_text = regex_replace_titles(file_text)
 	text = "[center]%s[/center]" % [file_text]
