ff5871bb0596318aa14522406db5568690dd4677
mgzu <2931170625@qq.com>
2025-07-03 23:16:26 +0800

    feat: Add language option

--- scenes/menus/options_menu/game/game_options_menu.gd
+++ scenes/menus/options_menu/game/game_options_menu.gd
@@ -2,3 +2,7 @@ extends Control
 
 func _on_ResetGameControl_reset_confirmed() -> void:
 	GlobalState.reset()
+
+
+func _on_language_control_setting_changed(value: Variant) -> void:
+	TranslationServer.set_locale(value)
