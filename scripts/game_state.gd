bdef2d6f4e3006376d83e3f9eb97ecafead023ab
Marek Belski <marek.belski@gmail.com>
2025-07-10 12:17:08 -0700

    Fixes creating game state when just checking it.

--- scripts/game_state.gd
+++ scripts/game_state.gd
@@ -10,7 +10,9 @@ const FILE_PATH = "res://scripts/game_state.gd"
 @export var times_played : int
 
 static func get_level_state(level_state_key : String) -> LevelState:
-	var game_state := get_game_state()
+	if not has_game_state(): 
+		return
+	var game_state := get_or_create_state()
 	if level_state_key.is_empty() : return
 	if level_state_key in game_state.level_states:
 		return game_state.level_states[level_state_key] 
@@ -22,39 +24,33 @@ static func get_level_state(level_state_key : String) -> LevelState:
 static func has_game_state() -> bool:
 	return GlobalState.has_state(STATE_NAME)
 
-static func get_game_state() -> GameState:
+static func get_or_create_state() -> GameState:
 	return GlobalState.get_state(STATE_NAME, FILE_PATH)
 
 static func get_current_level() -> int:
-	var game_state := get_game_state()
-	if not game_state: 
+	if not has_game_state(): 
 		return 0
+	var game_state := get_or_create_state()
 	return game_state.current_level
 
 static func get_max_level_reached() -> int:
-	var game_state := get_game_state()
-	if not game_state: 
+	if not has_game_state(): 
 		return 0
+	var game_state := get_or_create_state()
 	return game_state.max_level_reached
 
 static func level_reached(level_number : int) -> void:
-	var game_state := get_game_state()
-	if not game_state: 
-		return
+	var game_state := get_or_create_state()
 	game_state.max_level_reached = max(level_number, game_state.max_level_reached)
 	game_state.current_level = level_number
 	GlobalState.save()
 
 static func set_current_level(level_number : int) -> void:
-	var game_state := get_game_state()
-	if not game_state: 
-		return
+	var game_state := get_or_create_state()
 	game_state.current_level = level_number
 	GlobalState.save()
 
 static func start_game() -> void:
-	var game_state := get_game_state()
-	if not game_state: 
-		return
+	var game_state := get_or_create_state()
 	game_state.times_played += 1
 	GlobalState.save()
