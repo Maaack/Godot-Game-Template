537d2eb1d6f27fc0583ffdb0ed4be18049be7772
Marek Belski <marek.belski@gmail.com>
2025-10-15 20:59:17 -0700

    Moves example overlaid menus into the examples folder and overwrites old ones.

--- addons/maaacks_game_template/examples/scenes/overlaid_menus/level_won_menu.gd
+++ addons/maaacks_game_template/examples/scenes/overlaid_menus/level_won_menu.gd
@@ -1,1 +1,20 @@
-extends LevelWonMenu
+extends OverlaidMenu
+
+signal continue_pressed
+signal restart_pressed
+signal main_menu_pressed
+
+func _on_main_menu_button_pressed():
+	$ConfirmMainMenu.popup_centered()
+
+func _on_confirm_main_menu_confirmed():
+	main_menu_pressed.emit()
+	close()
+
+func _on_restart_button_pressed():
+	restart_pressed.emit()
+	close()
+
+func _on_close_button_pressed():
+	continue_pressed.emit()
+	close()
