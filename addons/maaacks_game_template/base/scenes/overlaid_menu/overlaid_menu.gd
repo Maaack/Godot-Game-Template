dab99efd03e53db73ac83bd3d1f41f12475ebad6
Marek Belski <marek.belski@gmail.com>
2025-10-15 18:56:57 -0700

    Makes window close ui_cancel release.

--- addons/maaacks_game_template/base/scenes/overlaid_menu/overlaid_menu.gd
+++ addons/maaacks_game_template/base/scenes/overlaid_menu/overlaid_menu.gd
@@ -29,7 +29,7 @@ func _handle_cancel_input() -> void:
 	close()
 
 func _unhandled_input(event : InputEvent) -> void:
-	if event.is_action_pressed("ui_cancel"):
+	if visible and event.is_action_released("ui_cancel"):
 		_handle_cancel_input()
 		get_viewport().set_input_as_handled()
 
