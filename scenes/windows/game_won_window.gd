f4cb2ac1a46b73587fd93acad2b5b3fe9edc65d8
Marek Belski <marek.belski@gmail.com>
2025-10-30 21:53:50 -0700

    Moves level progress windows from extras to examples.

--- scenes/windows/game_won_window.gd
+++ scenes/windows/game_won_window.gd
@@ -1,2 +1,26 @@
 @tool
-extends "res://addons/maaacks_game_template/extras/scenes/windows/game_won_window.gd"
+extends OverlaidWindow
+
+signal continue_pressed
+signal main_menu_pressed
+
+func _ready():
+	if OS.has_feature("web"):
+		%ExitButton.hide()
+
+func _on_exit_button_pressed():
+	%ExitConfirmation.show()
+
+func _on_main_menu_button_pressed():
+	%MainMenuConfirmation.show()
+
+func _on_close_button_pressed():
+	continue_pressed.emit()
+	close()
+
+func _on_main_menu_confirmation_confirmed():
+	main_menu_pressed.emit()
+	close()
+
+func _on_exit_confirmation_confirmed():
+	get_tree().quit()
--- scenes/windows/level_lost_window.gd
+++ scenes/windows/level_lost_window.gd
@@ -1,2 +1,26 @@
 @tool
-extends "res://addons/maaacks_game_template/extras/scenes/windows/level_lost_window.gd"
+extends OverlaidWindow
+
+signal restart_pressed
+signal main_menu_pressed
+
+func _ready():
+	if OS.has_feature("web"):
+		%ExitButton.hide()
+
+func _on_exit_button_pressed():
+	%ExitConfirmation.show()
+
+func _on_main_menu_button_pressed():
+	%MainMenuConfirmation.show()
+
+func _on_close_button_pressed():
+	restart_pressed.emit()
+	close()
+
+func _on_main_menu_confirmation_confirmed():
+	main_menu_pressed.emit()
+	close()
+
+func _on_exit_confirmation_confirmed():
+	get_tree().quit()
