633cd31aaea7b80e5415e95f853380ed69672ff4
Marek Belski <marek.belski@gmail.com>
2026-02-02 13:02:36 -0800

    Allow calling pause on pause menu controller.

--- addons/maaacks_game_template/base/nodes/utilities/pause_menu_controller.gd
+++ addons/maaacks_game_template/base/nodes/utilities/pause_menu_controller.gd
@@ -7,19 +7,22 @@ extends Node
 
 var pause_menu : Node
 
+func pause() -> void:
+	if pause_menu.visible: return
+	if not focused_viewport:
+		focused_viewport = get_viewport()
+	var _initial_focus_control = focused_viewport.gui_get_focus_owner()
+	pause_menu.show()
+	if pause_menu is CanvasLayer:
+		await pause_menu.visibility_changed
+	else:
+		await pause_menu.hidden
+	if is_inside_tree() and _initial_focus_control:
+		_initial_focus_control.grab_focus()
+
 func _unhandled_input(event : InputEvent) -> void:
 	if event.is_action_pressed("ui_cancel"):
-		if pause_menu.visible: return
-		if not focused_viewport:
-			focused_viewport = get_viewport()
-		var _initial_focus_control = focused_viewport.gui_get_focus_owner()
-		pause_menu.show()
-		if pause_menu is CanvasLayer:
-			await pause_menu.visibility_changed
-		else:
-			await pause_menu.hidden
-		if is_inside_tree() and _initial_focus_control:
-			_initial_focus_control.grab_focus()
+		pause()
 
 func _ready() -> void:
 	pause_menu = pause_menu_packed.instantiate()
