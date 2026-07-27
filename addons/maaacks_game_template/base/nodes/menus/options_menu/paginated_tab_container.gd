fcf2ad882a742633defe954968f3f9c4ee727470
Marek Belski <marek.belski@gmail.com>
2026-07-27 14:53:00 -0700

    Fix input cycling into a hidden tab.

--- addons/maaacks_game_template/base/nodes/menus/options_menu/paginated_tab_container.gd
+++ addons/maaacks_game_template/base/nodes/menus/options_menu/paginated_tab_container.gd
@@ -4,10 +4,14 @@ extends TabContainer
 func _unhandled_input(event : InputEvent) -> void:
 	if not is_visible_in_tree():
 		return
-	if event.is_action_pressed("ui_page_down"):
+	if event.is_action_pressed(&"ui_page_down"):
 		current_tab = (current_tab+1) % get_tab_count()
-	elif event.is_action_pressed("ui_page_up"):
+		while get("tab_%d/hidden" % current_tab):
+			current_tab = current_tab+1 % get_tab_count()
+	elif event.is_action_pressed(&"ui_page_up"):
 		if current_tab == 0:
 			current_tab = get_tab_count()-1
 		else:
+			current_tab = current_tab-1
+		while get("tab_%d/hidden" % current_tab):
 			current_tab = current_tab-1
