f44623442a1f9392ee09ee9a17a9b668273e2a65
Marek Belski <marek.belski@gmail.com>
2026-07-27 15:35:00 -0700

    Fixes some potential issues and cleans up code.

--- addons/maaacks_game_template/base/nodes/menus/options_menu/paginated_tab_container.gd
+++ addons/maaacks_game_template/base/nodes/menus/options_menu/paginated_tab_container.gd
@@ -4,14 +4,15 @@ extends TabContainer
 func _unhandled_input(event : InputEvent) -> void:
 	if (not is_visible_in_tree()) or tab_focus_mode == Control.FOCUS_NONE:
 		return
+	var is_first : bool = true
 	if event.is_action_pressed(&"ui_page_down"):
-		current_tab = (current_tab+1) % get_tab_count()
-		while get("tab_%d/hidden" % current_tab):
-			current_tab = current_tab+1 % get_tab_count()
+		while is_first or get("tab_%d/hidden" % current_tab):
+			is_first = false
+			current_tab = (current_tab+1) % get_tab_count()
 	elif event.is_action_pressed(&"ui_page_up"):
-		if current_tab == 0:
-			current_tab = get_tab_count()-1
-		else:
-			current_tab = current_tab-1
-		while get("tab_%d/hidden" % current_tab):
-			current_tab = current_tab-1
+		while is_first or get("tab_%d/hidden" % current_tab):
+			is_first = false
+			if current_tab == 0:
+				current_tab = get_tab_count()-1
+			else:
+				current_tab = current_tab-1
