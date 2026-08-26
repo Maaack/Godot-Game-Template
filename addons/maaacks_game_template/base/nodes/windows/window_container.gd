689d2f1b9178bfe1324b80058545433b3ca395b7
Marek Belski <marek.belski@gmail.com>
2026-08-26 11:11:34 -0700

    Removes opened signal from window container.

--- addons/maaacks_game_template/base/nodes/windows/window_container.gd
+++ addons/maaacks_game_template/base/nodes/windows/window_container.gd
@@ -3,7 +3,6 @@ class_name WindowContainer
 extends PanelContainer
 
 signal closed
-signal opened
 
 @export var ui_cancel_closes : bool = true
 
@@ -57,10 +56,6 @@ func _unhandled_input(event : InputEvent) -> void:
 
 func _on_close_button_pressed() -> void:
 	close()
-
-func show() -> void:
-	super.show()
-	opened.emit()
 
 func _exit_tree():
 	if Engine.is_editor_hint(): return
