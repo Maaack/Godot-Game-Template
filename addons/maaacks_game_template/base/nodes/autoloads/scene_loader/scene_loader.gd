d0d79c19f44ec90dabe8e5c2d2413160695db546
Marek Belski <marek.belski@gmail.com>
2026-08-29 18:26:00 -0700

    Copy changes from scene loader.

--- addons/maaacks_game_template/base/nodes/autoloads/scene_loader/scene_loader.gd
+++ addons/maaacks_game_template/base/nodes/autoloads/scene_loader/scene_loader.gd
@@ -6,7 +6,7 @@ signal scene_loaded
 
 ## Path to the loading screen to display to players while loading a scene.
 ## Will use ProjectSettings paths if left empty.
-@export_file("*.tscn") var loading_screen_path : String : set = set_loading_screen
+@export_file("*.tscn") var loading_screen_path : String : get = get_loading_screen_path, set = set_loading_screen_path
 
 @export_group("Debug")
 ## If true, enable debug mode.
@@ -70,14 +70,16 @@ func change_scene_to_loading_screen() -> void:
 		push_error("failed to change scenes to loading screen: %d" % err)
 		get_tree().quit()
 
-func set_loading_screen(value : String) -> void:
+func set_loading_screen_path(value : String) -> void:
 	loading_screen_path = value
-	var _loading_screen_path := loading_screen_path
-	if _loading_screen_path.is_empty():
-		_loading_screen_path = MaaacksGameTemplate.get_loading_scene_path()
-	if _loading_screen_path.is_empty():
+	if loading_screen_path.is_empty():
 		return
-	_loading_screen = load(_loading_screen_path)
+	_loading_screen = load(loading_screen_path)
+
+func get_loading_screen_path() -> String:
+	if loading_screen_path.is_empty():
+		return MaaacksGameTemplate.get_loading_scene_path()
+	return loading_screen_path
 
 func is_loading_scene(check_scene_path) -> bool:
 	return check_scene_path == _scene_path
