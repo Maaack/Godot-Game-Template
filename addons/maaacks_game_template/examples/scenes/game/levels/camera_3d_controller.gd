1cb383ac522cc8ba3ea8d65bb27e48608426f81c
Marek Belski <marek.belski@gmail.com>
2026-09-06 22:31:22 -0700

    Adds a basic 3d camera controller.

---
+++ addons/maaacks_game_template/examples/scenes/game/levels/camera_3d_controller.gd
@@ -0,0 +1,22 @@
+extends Node3D
+
+@export var base_mouse_sensitivity : float = 0.001
+@export var base_joypad_sensitivity : float = 1.000
+@export_node_path("CharacterBody3D") var character_body_node_path : NodePath = ^".."
+@onready var character_body : CharacterBody3D = get_node_or_null(character_body_node_path)
+
+var _rotation_rate : float = 0.0
+
+func _input(event):
+	if event is InputEventMouseMotion:
+		var mouse_sensitivity = PlayerConfig.get_config(AppSettings.INPUT_SECTION, &"MouseSensitivity", 1.0)
+		mouse_sensitivity *= base_mouse_sensitivity
+		character_body.rotation.y -= event.relative.x * mouse_sensitivity
+	if event is InputEventJoypadMotion:
+		if event.axis == JoyAxis.JOY_AXIS_RIGHT_X:
+			var joypad_sensitivity = PlayerConfig.get_config(AppSettings.INPUT_SECTION, &"JoypadSensitivity", 1.0)
+			joypad_sensitivity *= base_joypad_sensitivity
+			_rotation_rate = event.axis_value * joypad_sensitivity
+
+func _process(delta):
+	character_body.rotation.y -= _rotation_rate * delta
