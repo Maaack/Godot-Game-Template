72eef688a3c6e95c34745fd3f14642d3a71393ba
Marek Belski <marek.belski@gmail.com>
2026-09-07 09:50:11 -0700

    Update characters and tutorials to use default ui inputs.

--- addons/maaacks_game_template/examples/scenes/game/levels/character_body_2d.gd
+++ addons/maaacks_game_template/examples/scenes/game/levels/character_body_2d.gd
@@ -11,12 +11,12 @@ func _physics_process(delta):
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("move_left", "move_right")
+	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
--- addons/maaacks_game_template/examples/scenes/game/levels/character_body_3d.gd
+++ addons/maaacks_game_template/examples/scenes/game/levels/character_body_3d.gd
@@ -11,12 +11,12 @@ func _physics_process(delta):
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
+	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
