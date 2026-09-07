2f44a0e1b06182da8b8324ed8ca0ba5208754f91
Marek Belski <marek.belski@gmail.com>
2026-09-07 13:30:20 -0700

    Fix new levels not sending next level path on win.

--- scenes/game/levels/level_2.gd
+++ scenes/game/levels/level_2.gd
@@ -4,4 +4,4 @@ func _on_lose_area_2d_body_entered(_node: Node2D) -> void:
 	level_lost.emit()
 
 func _on_win_area_2d_body_entered(_node: Node2D) -> void:
-	level_won.emit()
+	level_won.emit(next_level_path)
--- scenes/game/levels/level_3.gd
+++ scenes/game/levels/level_3.gd
@@ -4,4 +4,4 @@ func _on_lose_area_3d_body_entered(_body: Node3D) -> void:
 	level_lost.emit()
 
 func _on_win_area_3d_body_entered(_body: Node3D) -> void:
-	level_won.emit()
+	level_won.emit(next_level_path)
