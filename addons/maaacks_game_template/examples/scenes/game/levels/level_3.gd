ebbe9d33131494826ae0163110c563ab58d8da8d
Marek Belski <marek.belski@gmail.com>
2026-09-07 09:55:24 -0700

    Fix potential warnings.

--- addons/maaacks_game_template/examples/scenes/game/levels/level_3.gd
+++ addons/maaacks_game_template/examples/scenes/game/levels/level_3.gd
@@ -1,7 +1,7 @@
 extends "level.gd"
 
-func _on_lose_area_3d_body_entered(body):
+func _on_lose_area_3d_body_entered(_body: Node3D) -> void:
 	level_lost.emit()
 
-func _on_win_area_3d_body_entered(body):
+func _on_win_area_3d_body_entered(_body: Node3D) -> void:
 	level_won.emit()
